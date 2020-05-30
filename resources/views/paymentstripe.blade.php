d33eb5d231fc9d84953bb79c3e71e594f828d7a8
Supriyo Mitra <smitra@artinfo.com>
2020-05-30 13:43:55 +0530

    Beautified the stripe card info form

--- resources/views/paymentstripe.blade.php
+++ resources/views/paymentstripe.blade.php
@@ -1,189 +1,129 @@
 <!DOCTYPE html>
-<html lang="en">
- <head>
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-   <title>Stripe Card Payment Form</title>
-   <!-- Bootstrap -->
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
- <style>
- .submit-button {
- margin-top: 10px;
-}
-
-body { margin:50px auto; width:600px;}
-
-/* CSS for Credit Card Payment form */
-.credit-card-box .panel-title {
-    display: inline;
-    font-weight: bold;
-}
-.credit-card-box .form-control.error {
-    border-color: red;
-    outline: 0;
-    box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(255,0,0,0.6);
-}
-.credit-card-box label.error {
-  font-weight: bold;
-  color: red;
-  padding: 2px 8px;
-  margin-top: 2px;
-}
-.credit-card-box .payment-errors {
-  font-weight: bold;
-  color: red;
-  padding: 2px 8px;
-  margin-top: 2px;
-}
-.credit-card-box label {
-    display: block;
-}
-/* The old "center div vertically" hack */
-.credit-card-box .display-table {
-    display: table;
-}
-.credit-card-box .display-tr {
-    display: table-row;
-}
-.credit-card-box .display-td {
-    display: table-cell;
-    vertical-align: middle;
-    width: 50%;
-}
-/* Just looks nicer */
-.credit-card-box .panel-heading img {
-    min-width: 180px;
-}
- </style>
- </head>
- <body>
+<html>
+<head>
+	<title>Stripe Payment Gateway</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <style type="text/css">
+        .panel-title {
+        display: inline;
+        font-weight: bold;
+        }
+        .display-table {
+            display: table;
+        }
+        .display-tr {
+            display: table-row;
+        }
+        .display-td {
+            display: table-cell;
+            vertical-align: middle;
+            width: 61%;
+        }
+    </style>
+</head>
+<body>
+  
 <div class="container">
-<div class="row">
-<!-- You can make it whatever width you want. I'm making it full width
-on <= small devices and 4/12 page width on >= medium devices -->
-<div class="col-xs-12 col-md-4">
+  
+    <h1>Stripe Payment Gateway</h1>
+  
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default credit-card-box">
+                <div class="panel-heading display-table" >
+                    <div class="row display-tr" >
+                        <h3 class="panel-title display-td" >Payment Details</h3>
+                        <div class="display-td" >                            
+                            <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
+                        </div>
+                    </div>                    
+                </div>
+                <div class="panel-body">
+  
+                    @if (Session::has('success'))
+                        <div class="alert alert-success text-center">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                            <p>{{ Session::get('success') }}</p>
+                        </div>
+                    @endif
+  
+                    <form role="form" action="/cardpayment" method="post" class="require-validation" data-cc-on-file="false" id="payment-form">
+                        {{ csrf_field() }}
+  
+                        <div class='form-row row'>
+                            <div class='col-xs-12 form-group required'>
+                                <label class='control-label'>Name on Card</label> <input class='form-control' size='4' type='text' name="customername">
+                            </div>
+                        </div>
+  
+                        <div class='form-row row'>
+                            <div class='col-xs-12 form-group card required'>
+                                <label class='control-label'>Card Number</label> <input
+                                    autocomplete='off' class='form-control card-number' size='20' type='text' name="card_no">
+                            </div>
+                        </div>
+  
+                        <div class='form-row row'>
+                            <div class='col-xs-12 col-md-4 form-group cvc required'>
+                                <label class='control-label'>CVC</label> <input autocomplete='off'
+                                    class='form-control card-cvc' placeholder='ex. 311' size='4' type='text' name="cvvNumber">
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                                <label class='control-label'>Expiration Month</label> <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text' name="ccExpiryMonth">
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                                <label class='control-label'>Expiration Year</label> <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text' name="ccExpiryYear">
+                            </div>
+                        </div>
 
+			<div class='form-row row'>
+                            <div class='col-xs-12 form-group required'>
+                                <label class='control-label'>Address Line 1</label> <input class='form-control' size='4' type='text' name="addressline1">
+                            </div>
+                        </div>
 
-<!-- CREDIT CARD FORM STARTS HERE -->
-<div class="panel panel-default credit-card-box">
-<div class="panel-heading display-table" >
-<div class="row display-tr" >
-<h3 class="panel-title display-td" >Payment Details</h3>
-<div class="display-td" >                            
-<img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
-</div>
-</div>                    
-</div>
-<div class="panel-body">
-<form role="form" id="payment-form" method="POST" action="/cardpayment">
-{{ csrf_field() }}
-<div class="row">
-<div class="col-xs-12">
-<div class="form-group">
-<label for="customername">Name on Card</label>
-<div class="input-group">
-<input type="tel" class="form-control" name="customername"  placeholder="Name on card" autocomplete="cc-number" />
-<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
-</div>
-</div>                            
-</div>
+			<div class='form-row row'>
+                            <div class='col-xs-12 form-group required'>
+                                <label class='control-label'>Address Line 2</label> <input class='form-control' size='4' type='text' name="addressline2">
+                            </div>
+                        </div>
 
-<div class="row">
-<div class="col-xs-12">
-<div class="form-group">
-<label for="card_no">Card Number</label>
-<div class="input-group">
-<input type="tel" class="form-control" name="card_no"  placeholder="Valid Card Number" autocomplete="cc-number" required autofocus />
-<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
+			<div class='form-row row'>
+                            <div class='col-xs-12 col-md-4 form-group cvc required'>
+                                <label class='control-label'>City</label> <input autocomplete='off'
+                                    class='form-control card-cvc' size='4' type='text' name="city">
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                                <label class='control-label'>Country</label> <input class='form-control card-expiry-month' size='6' type='text' name="country">
+                            </div>
+                        </div>
+  
+                        <div class='form-row row'>
+                            <div class='col-md-12 error form-group hide'>
+                                <div class='alert-danger alert'>Please correct the errors and try
+                                    again.</div>
+                            </div>
+                        </div>
+  
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay <?php echo number_format(round($imageprice,2), 2); ?></button>
+                            </div>
+                        </div>
+                        <input type='hidden' name='payamt' value="<?php echo number_format(round($imageprice,2), 2); ?>">
+			<input type='hidden' name='lowrespath' value="<?php echo $lowrespath; ?>">
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+      
 </div>
-</div>                            
-</div>
-
-<div class="row">
-<div class="col-xs-4">
-<div class="form-group">
-<label><span class="hidden-xs">Expiration Month/Year</span><span class="visible-xs-inline"></span></label>
-<input type="tel" class="form-control" name="ccExpiryMonth" placeholder="MM" autocomplete="cc-exp" size=4 required />
-</div>
-</div>
-/
-<div class="col-xs-4">
-<div class="form-group">
-<input type="tel" class="form-control" name="ccExpiryYear" placeholder="YYYY" autocomplete="cc-exp" size=4 required />
-</div>
-</div>
-</div>
-<div class="col-xs-5">
-<div class="form-group">
-<label for="cardCVC">CVV Code</label>
-<input type="tel" class="form-control" name="cvvNumber" placeholder="CVC" autocomplete="cc-csc" required />
-<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
-</div>
-</div>
-
-<div class="row">
-<div class="col-xs-12">
-<div class="form-group">
-<label for="cardNumber">Address Line 1</label>
-<div class="input-group">
-<input type="tel" class="form-control" name="addressline1"  placeholder="Address line 1" size=12 />
-</div>
-</div>                            
-</div>
-
-<div class="row">
-<div class="col-xs-12">
-<div class="form-group">
-<label for="cardNumber">Address Line 2</label>
-<div class="input-group">
-<input type="tel" class="form-control" name="addressline2"  placeholder="Address line 2" size=12 />
-</div>
-</div>                            
-</div>
-
-<div class="row">
-<div class="col-xs-4">
-<div class="form-group">
-<label for="cardExpiry"><span class="hidden-xs">City</span><span class="visible-xs-inline"></span></label>
-<input type="tel" class="form-control" name="city" autocomplete="cc-exp" size=8 required />
-</div>
-</div>
-<div class="col-xs-4">
-<div class="form-group">
-<label for="cardExpiry"><span class="hidden-xs">Country</span><span class="visible-xs-inline"></span></label>
-<input type="tel" class="form-control" name="country" autocomplete="cc-exp" size=8 required />
-</div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-xs-12">
-<button class="btn btn-success btn-lg btn-block" type="submit">Pay <?php echo number_format(round($imageprice,2), 2); ?></button>
-</div>
-</div>
-<div class="row" style="display:none;">
-<div class="col-xs-12">
-<p class="payment-errors"></p>
-</div>
-</div>
-<input type='hidden' name='payamt' value="<?php echo number_format(round($imageprice,2), 2); ?>">
-<input type='hidden' name='lowrespath' value="<?php echo $lowrespath; ?>">
-</form>
-</div>
-</div>            
-<!-- CREDIT CARD FORM ENDS HERE -->
-
-</div>            
-
-</div>
-</div>
-
+  
 </body>
+  
+
+</html>
 
 
